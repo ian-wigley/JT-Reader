d94325c50b1536e806c60213d22a8ab95996da79
Ian Wigley <ian.wigley@outlook.com>
2023-12-27 09:14:42 +0000

    Create LogicalSceneGraphNode.cpp

---
+++ V7/JT-Loader_Vis-Studio_C++/JTLoader/LogicalSceneGraphNode.cpp
@@ -0,0 +1,17 @@
+
+#include "LogicalSceneGraphNode.h"
+
+LogicalSceneGraphNode::LogicalSceneGraphNode(void)
+{
+	// To-do - Traverse from here to find all the relevant nodes etc
+}
+
+LogicalSceneGraphNode::~LogicalSceneGraphNode(void)
+{
+}
+
+void LogicalSceneGraphNode::populateData(void)
+{
+}
+
+
