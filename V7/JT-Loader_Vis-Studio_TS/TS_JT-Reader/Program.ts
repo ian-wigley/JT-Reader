9e4e1f07ee24f1c3761541d0f0f5023f06ba847b
Ian Wigley <ian.wigley@outlook.com>
2023-12-31 11:13:49 +0000

    Add Nodes

---
+++ V7/JT-Loader_Vis-Studio_TS/TS_JT-Reader/Program.ts
@@ -0,0 +1,9 @@
+﻿module C_sharp_JT_Reader {
+    export class Program {
+        static Main(): void {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
