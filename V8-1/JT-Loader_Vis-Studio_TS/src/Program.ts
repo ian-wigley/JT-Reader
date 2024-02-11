a6d12fa149fa944eabff1655d9f39f0ae24ab964
Ian Wigley <ian.wigley@outlook.com>
2024-02-11 17:16:27 +0000

    Project rename

--- V8-1/JT-Loader_Vis-Studio_TS/src/Program.ts
+++
@@ -1,9 +0,0 @@
-﻿module C_sharp_JT_Reader {
-    export class Program {
-        static Main(): void {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
-}
