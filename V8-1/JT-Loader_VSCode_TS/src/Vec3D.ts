736c8b9979640def694be3ec4529087bf927d43d
Ian Wigley <ian.wigley@outlook.com>
2024-02-14 18:04:20 +0000

    Create Vec3D.ts

---
+++ V8-1/JT-Loader_VSCode_TS/src/Vec3D.ts
@@ -0,0 +1,35 @@
+﻿export class Vec3D {
+    x: number;
+    y: number;
+    z: number;
+
+    constructor(x: number, y: number, z: number) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public getX(): number {
+        return this.x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+
+    public getZ(): number {
+        return this.z;
+    }
+
+    public getXf(): number {
+        return this.x;
+    }
+
+    public getYf(): number {
+        return this.y;
+    }
+
+    public getZf(): number {
+        return this.z;
+    }
+}
