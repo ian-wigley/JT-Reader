f45749d2efc0062ff5255a9283676670756f53d0
Ian Wigley <ian.wigley@outlook.com>
2024-02-15 16:22:55 +0000

    Create VecI32.ts

---
+++ V8-1/JT-Loader_VSCode_TS/src/VecI32.ts
@@ -0,0 +1,12 @@
+﻿export class VecI32 {
+    private _counter: number = 0;
+    private _data: number[];
+
+    public setLength(size: number): void {
+        this._data = new Array(size);
+    }
+
+    public ptr(): number {
+        return this._counter++;
+    }
+}
