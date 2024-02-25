294ba632b3938adee3c0a5526a53c43743168fbc
Ian Wigley <ian.wigley@outlook.com>
2024-02-25 19:47:55 +0000

    Create MetaDataNode.ts

---
+++ V8-1/JT-Loader_VSCode_TS/src/MetaDataNode.ts
@@ -0,0 +1,21 @@
+﻿export class MetaDataNode extends GroupJTNode {
+
+    protected elementLength: number;
+    compare: string;
+    _propertyProxyNode: PropertyProxyMetaData;
+
+    constructor(fileVersion: number, richTextBox: List<string>) {
+        _fileVersion = fileVersion;
+        this.m_textBox = richTextBox;
+    }
+
+    public populateData(uncompressed: number[], filePosCount: number): number {
+        _uncompressed = uncompressed;
+        _filePosCount = filePosCount;
+        _filePosCount = TraverseGroupNode(_richTextBox, _filePosCount);
+        Buffer.BlockCopy(_uncompressed, _filePosCount, fileBytes, 0, 4);
+        _versionNumber = DataTypes.getInt16(fileBytes);
+        _filePosCount += 2;
+        return this._filePosCount;
+    }
+}
