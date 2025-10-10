6ce8e6a494a197b0ac0f4503ca2f22b1d203b19a
Ian Wigley <ian.wigley@outlook.com>
2025-10-10 18:50:01 +0100

    Create GroupJTNode.ts

---
+++ V8-1/JT-Loader_VSCode_TS/src/GroupJTNode.ts
@@ -0,0 +1,26 @@
+﻿import { BaseJTNode } from "./BaseJTNode";
+import { Buffer } from "./BitBuffer";
+import { DataTypes } from "./DataTypes";
+
+export class GroupJTNode extends BaseJTNode {
+
+    constructor(fileVersion: number) {
+        super();
+        this._fileVersion = fileVersion;
+    }
+
+    public TraverseGroupNode(filePosCount: number): number {
+        this._filePosCount = filePosCount;
+        this._filePosCount = this.TraverseBaseNodeData();
+        Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
+        var _childCount: Int32 = DataTypes.getInt32(this.fileBytes);
+        this._filePosCount += 4;
+        var _childNodeObjectID: Int32[] = new Array(_childCount);
+        for (var i: number = 0; i < _childCount; i++) {
+            Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
+            _childNodeObjectID[i] = DataTypes.getInt32(this.fileBytes);
+            this._filePosCount += 4;
+        }
+        return this._filePosCount;
+    }
+}
