14cc3b7777aa62f79723780093ac2e531bc59c95
Ian Wigley <ian.wigley@outlook.com>
2025-10-11 09:39:19 +0100

    Create InstanceNodeElement.ts

---
+++ V8-1/JT-Loader_VSCode_TS/src/InstanceNodeElement.ts
@@ -0,0 +1,21 @@
+﻿import { Buffer } from "./BitBuffer";
+import { DataTypes } from "./DataTypes";
+import { GroupJTNode } from "./GroupJTNode";
+
+export class InstanceNodeElement extends GroupJTNode {
+
+    constructor(fileVersion: number) {
+        super(fileVersion);
+        // this._fileVersion = fileVersion;
+    }
+
+    public populateData(uncompressed: number[], filePosCount: number): number {
+        this._uncompressed = uncompressed;
+        this._filePosCount = filePosCount;
+        this._filePosCount = this.TraverseBaseNodeData();
+        Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
+        var _childNodeObjectID: Int32 = DataTypes.getInt32(this.fileBytes);
+        this._filePosCount += 4;
+        return this._filePosCount;
+    }
+}
