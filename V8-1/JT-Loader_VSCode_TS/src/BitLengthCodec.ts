c57d1a7a04dfcd9697427de599f4c0e207ef9135
Ian Wigley <ian.wigley@outlook.com>
2025-10-12 10:50:46 +0100

    Create BitLengthCodec.ts

---
+++ V8-1/JT-Loader_VSCode_TS/src/BitLengthCodec.ts
@@ -0,0 +1,47 @@
+﻿
+export class BitLengthCodec {
+
+    public DecompressBitLength(encodedBytes: any /*Int32[]*/, codeTextLength: number, numSymbolsToRead: number): number[] {
+        var encodedBits: any;//BitBuffer = new BitBuffer(encodedBytes);
+        var bitFieldWith: number = 0;
+        var result: number[] = new Array(numSymbolsToRead);
+        var position: number = 0;
+        while (encodedBits.getBitPos() < codeTextLength) {
+            if (encodedBits.readAsInt(1) == 0) {
+                var decodedSymbol: number = -1;
+                if (bitFieldWith == 0) {
+                    decodedSymbol = 0;
+                }
+                else {
+                    decodedSymbol = encodedBits.readAsInt(bitFieldWith);
+                    decodedSymbol <<= (32 - bitFieldWith);
+                    decodedSymbol >>= (32 - bitFieldWith);
+                }
+                result[position++] = decodedSymbol;
+            }
+            else {
+                var adjustmentBit: number = encodedBits.readAsInt(1);
+                do {
+                    if (adjustmentBit == 1) {
+                        bitFieldWith += 2;
+                    }
+                    else {
+                        bitFieldWith -= 2;
+                    }
+                }
+                while (encodedBits.readAsInt(1) == adjustmentBit);
+                var decodedSymbol: number = -1;
+                if (bitFieldWith == 0) {
+                    decodedSymbol = 0;
+                }
+                else {
+                    decodedSymbol = encodedBits.readAsInt(bitFieldWith);
+                    decodedSymbol <<= (32 - bitFieldWith);
+                    decodedSymbol >>= (32 - bitFieldWith);
+                }
+                result[position++] = decodedSymbol;
+            }
+        }
+        return result;
+    }
+}
