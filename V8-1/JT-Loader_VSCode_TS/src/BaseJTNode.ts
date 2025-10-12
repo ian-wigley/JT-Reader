3ea190accffe9200dc9248e6016fa2e3fc64e790
Ian Wigley <ian.wigley@outlook.com>
2025-10-12 10:50:54 +0100

    Update BaseJTNode.ts

--- V8-1/JT-Loader_VSCode_TS/src/BaseJTNode.ts
+++ V8-1/JT-Loader_VSCode_TS/src/BaseJTNode.ts
@@ -1,4 +1,4 @@
-﻿export class BaseJTNode {
+﻿﻿export class BaseJTNode {
 
     protected _uncompressed: number[];
     protected _fileVersion: number = 0;
@@ -12,29 +12,30 @@
 
     protected TraverseBaseNodeData(): number {
         if (this._fileVersion >= 9.5) {
-            Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
-            this._versionNumber = DataTypes.getInt16(this.fileBytes);
+            // Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
+            // this._versionNumber = DataTypes.getInt16(this.fileBytes);
             this._filePosCount += 2;
         }
         else {
-            Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
-            this._objectID = DataTypes.getInt32(this.fileBytes);
+            // Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
+            // this._objectID = DataTypes.getInt32(this.fileBytes);
             this._filePosCount += 4;
         }
-        Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
-        this._nodeFlags = DataTypes.getUInt32(this.fileBytes);
+
+        // Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
+        // this._nodeFlags = DataTypes.getUInt32(this.fileBytes);
         let a: number = this.fileBytes[0];
         let b: number = this.fileBytes[2];
         let c: number = (b | a);
         this._filePosCount += 4;
         if (this._nodeFlags == 0) {
-            Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
-            this._attributeCount = DataTypes.getInt32(this.fileBytes);
+            // Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
+            // this._attributeCount = DataTypes.getInt32(this.fileBytes);
             this._filePosCount += 4;
             this._attributeObjectID = new Array(this._attributeCount);
             for (let i: number = 0; i < this._attributeCount; i++) {
-                Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
-                this._attributeObjectID[i] = DataTypes.getInt32(this.fileBytes);
+                // Buffer.BlockCopy(this._uncompressed, this._filePosCount, this.fileBytes, 0, 4);
+                // this._attributeObjectID[i] = DataTypes.getInt32(this.fileBytes);
                 this._filePosCount += 4;
             }
         }
