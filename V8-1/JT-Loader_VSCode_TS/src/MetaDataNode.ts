fd5b5c87240253eb2cc695a13265c8aa4d4a1a3f
Ian Wigley <ian.wigley@outlook.com>
2025-10-12 10:49:12 +0100

    Update MetaDataNode.ts

---
+++ V8-1/JT-Loader_VSCode_TS/src/MetaDataNode.ts
@@ -0,0 +1,16 @@
+import { BaseJTNode } from "./BaseJTNode";
+
+export class MetaDataNode  extends BaseJTNode {
+
+    constructor(fileVersion: number) {
+        super();
+        // this._fileVersion = fileVersion;
+    }
+
+    public populateData(uncompressed: number[], _filePosCount: number): number {
+        // this._uncompressed = uncompressed;
+        // this._filePosCount = filePosCount;
+        // this.elementLength = elementLength;
+        return 0;
+    }
+}
