8a458936748a075e6211e50860c0d89ab7030215
Ian Wigley <ian.wigley@outlook.com>
2025-10-13 17:06:08 +0100

    Update GUID.ts

--- V8-1/JT-Loader_VSCode_TS/src/GUID.ts
+++ V8-1/JT-Loader_VSCode_TS/src/GUID.ts
@@ -1,16 +1,19 @@
+import { Convert } from "./Convert.js";
+
 export class Guid {
 
     _one: number = 0;
     _two: number = 0;
     _three: number = 0;
-    _four: number = 0;
-    _five: number = 0;
-    _six: number = 0;
-    _seven: number = 0;
-    _eight: number = 0;
-    _nine: number = 0;
-    _ten: number = 0;
-    _eleven: number = 0;
+    _four: string = "00";
+    _five: string = "00";
+    _six: string = "00";
+    _seven: string = "00";
+    _eight: string = "00";
+    _nine: string = "00";
+    _ten: string = "00";
+    _eleven: string = "00";
+    guid: string = "";
 
     //Guid(0x10dd1035, 0x2ac8, 0x11d1, 0x9b, 0x6b, 0x00, 0x80, 0xc7, 0xbb, 0x59, 0x97)
     constructor(one: any, two: number = 0, three: number = 0,
@@ -20,14 +23,16 @@ export class Guid {
         this._one = one;
         this._two = two;
         this._three = three;
-        this._four = four;
-        this._five = five;
-        this._six = six;
-        this._seven = seven;
-        this._eight = eight;
-        this._nine = nine;
-        this._ten = ten;
-        this._eleven = eleven;
+        this._four = Convert.X2(four);
+        this._five = Convert.X2(five);
+        this._six = Convert.X2(six);
+        this._seven = Convert.X2(seven);
+        this._eight = Convert.X2(eight);
+        this._nine = Convert.X2(nine);
+        this._ten = Convert.X2(ten);
+        this._eleven = Convert.X2(eleven);
+        this.guid = this._one + "-" + this._two + "-" + this._three + "-" + this._four + "-" + this._five + "-" +
+            this._six + "-" + this._seven + "-" + this._eight + "-" + this._nine + "-" + this._ten + "-" + this._eleven;
     }
 
 }
